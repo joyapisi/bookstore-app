8851a7a136df7b123c601b4ed455a64334f93fbf
Joy Phoebe <joyapisi@gmail.com>
2023-05-09 16:24:10 +0300

    add reducers in booksSlice.js and categoriesSlice.js

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,30 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  value: 0,
+  categories: [],
 };
 
 export const categoriesSlice = createSlice({
   name: 'categories',
   initialState,
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
-    },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+    checkUp: (state) => {
+      const { length } = state.categories;
+      if (length === 0) {
+        state.categories = 'Under construction';
+      }
     },
   },
 });
 
-// Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const { checkUp } = categoriesSlice.actions;
 
-export default counterSlice.reducer;
+export default categoriesSlice.reducer;
