8e03de3de1b53cdaea1dd248f5d00eeaa7437926
Joy Phoebe <joyapisi@gmail.com>
2023-05-09 19:25:33 +0300

    📥import categoriesReducer to store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
 
 const store = configureStore({
   reducer: {
-    books: booksReducer,
+    books: booksReducer, categories: categoriesReducer,
   },
 });
 
