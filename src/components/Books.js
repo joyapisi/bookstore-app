0219cc5879688807f9fd47126961c40dd11ff4b2
Joy Phoebe <joyapisi@gmail.com>
2023-05-10 20:15:57 +0300

    ⚒ Fix styling in UI

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,15 +1,9 @@
 import ListofBooks from './ListofBooks';
-import AddaBook from './AddaBook';
-import Sample from './Sample';
 
 export default function Books() {
   return (
-    <>
-      <article className="whole-body">
-        <ListofBooks />
-        <AddaBook />
-        <Sample />
-      </article>
-    </>
+    <div className="whole-body">
+      <ListofBooks />
+    </div>
   );
 }
