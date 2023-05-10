a223ed01e6a01918a48ba21fd5b9c356e730be17
Joy Phoebe <joyapisi@gmail.com>
2023-05-10 20:16:40 +0300

    👨🏽‍💻 Fix linter errors

--- src/components/ListofBooks.js
+++ src/components/ListofBooks.js
@@ -25,10 +25,9 @@ function ListofBooks() {
 
   return (
     <>
-      <div className="outer-container">
-
-        <div id="books-list">
-          {bookItems.map((book) => (
+      <div className="books-list">
+        {bookItems.map((book) => (
+          <>
             <EachBook
               key={book.item_id}
               title={book.title}
@@ -36,37 +35,9 @@ function ListofBooks() {
               category={book.category}
               id={book.item_id}
             />
-          ))}
-        </div>
-
-        {/* <div className="bookmark">
-          <svg className="bookmark-progress" xmlns="http://www.w3.org/2000/svg">
-            <circle
-              className="bookmark-progress-gray"
-              cx="111"
-              cy="100"
-              r="44"
-            />
-            <circle
-              className="bookmark-progress-blue"
-              cx="111"
-              cy="100"
-              r="44"
-            />
-          </svg>
-          <div className="percentage-complete">
-            <h1 className="percent">64%</h1>
-            <p className="state">Completed</p>
-          </div>
-        </div>
-
-        <div className="inner-container">
-          <p>CURRENT CHAPTER</p>
-          <p>Chapter 17</p>
-          <button type="button">UPDATE PROGRESS</button>
-        </div>
-
-      </div> */}
+          </>
+        ))}
+      </div>
 
       <div className="input-form">
         <h1>ADD NEW BOOK</h1>
@@ -84,63 +55,3 @@ function ListofBooks() {
 }
 
 export default ListofBooks;
-
-
-{/* return (
-    <>
-      <div className="outer-container">
-
-        <div id="books-list">
-          {bookItems.map((book) => (
-            <EachBook
-              key={book.item_id}
-              title={book.title}
-              author={book.author}
-              category={book.category}
-              id={book.item_id}
-            />
-          ))}
-        </div>
-
-        <div className="bookmark">
-          <svg className="bookmark-progress" xmlns="http://www.w3.org/2000/svg">
-            <circle
-              className="bookmark-progress-gray"
-              cx="111"
-              cy="100"
-              r="44"
-            />
-            <circle
-              className="bookmark-progress-blue"
-              cx="111"
-              cy="100"
-              r="44"
-            />
-          </svg>
-          <div className="percentage-complete">
-            <h1 className="percent">64%</h1>
-            <p className="state">Completed</p>
-          </div>
-        </div>
-
-        <div className="inner-container">
-          <p>CURRENT CHAPTER</p>
-          <p>Chapter 17</p>
-          <button type="button">UPDATE PROGRESS</button>
-        </div>
-
-      </div>
-
-      <div className="input-form">
-        <h1>ADD NEW BOOK</h1>
-        <form>
-          <input value={values.title || ''} type="text" name="title" placeholder="Book title" onChange={handleChange} />
-          <input value={values.author || ''} type="text" name="author" placeholder="Book author" onChange={handleChange} />
-          <select name="text" placeholder="Category">
-            <option value="category"> Category </option>
-          </select>
-          <button id="Add Button" type="button" onClick={handleSubmit}>ADD BOOK</button>
-        </form>
-      </div>
-    </>
-  ); */}
